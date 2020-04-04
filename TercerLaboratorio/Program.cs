2de588ac6229a675f5559aaafdc888d2080a6d96
Lucas Moyano Rivas <lucasmoyanor@Lucass-MacBook-Pro-2.local>
2020-04-04 01:28:04 -0300

    Primer repositorio del lab 3

---
+++ TercerLaboratorio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TercerLaboratorio
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
